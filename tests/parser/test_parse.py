261fca645bc197fe3dcfee5ddd4074de9e932f01
Konstantin Gründger <kammermark@gmx.de>
2025-05-05 09:48:01 +0200

    test the rust parser

--- tests/parser/test_parse.py
+++ tests/parser/test_parse.py
@@ -81,7 +81,7 @@ def test_naviter_beacons():
 
 
 def test_delay_beacons():
-    _parse_valid_beacon_data_file(filename='OGNDELAY_Delay.txt', beacon_type='unknown')
+    _parse_valid_beacon_data_file(filename='OGNDELAY_Delay.txt', beacon_type='tracker')
 
 
 def test_wx_beacons():
